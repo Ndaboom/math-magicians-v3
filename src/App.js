d34a2bf4605edc38258e96e4e32c2d65550f0c2d
ndaboom <ndabosam084@gmail.com>
2023-03-24 13:49:25 +0100

    Quotes component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
-    <Calculator name="Calculator 1.0" />
+    <>
+      <Calculator name="Calculator 1.0" />
+      <Quotes />
+    </>
   );
 }
 
---
+++ src/components/Quotes.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Quotes() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const category = 'happiness';
+  const apiKey = '3d8V6/tgQg33DNwFGRJhSQ==ag37Ez9EP2ZdaxPN';
+
+  useEffect(() => {
+    axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      headers: {
+        'X-Api-Key': apiKey,
+      },
+    })
+      .then((response) => {
+        const data = response.data[0];
+        setQuote(data.quote);
+        setAuthor(data.author);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError('Error while displaying quotes');
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p className="state-display">Loading...</p>;
+  }
+  if (error) {
+    return (
+      <em className="state-display">
+        {error}
+        {' '}
+        ... Please refresh the page.
+      </em>
+    );
+  }
+
+  return (
+    <div className="quotes-box">
+      <blockquote>
+        { quote }
+        <cite>
+          -
+          {' '}
+          { author }
+        </cite>
+      </blockquote>
+    </div>
+  );
+}
+
+export default Quotes;
